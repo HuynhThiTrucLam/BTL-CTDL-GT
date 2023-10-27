90aaabef3840acb6775a85450ee620abab0846bf
Võ Công Sinh <congsinhv@gmail.com>
2023-10-27 14:02:14 +0700

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(585, 900), "SFML Image Loading");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("images/background.png")) {
+        // Xử lý trường hợp tải hình ảnh không thành công
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return 0;
+}
